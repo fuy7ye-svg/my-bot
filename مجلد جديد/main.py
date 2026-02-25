6496df3a4a2425da1afeca6210c154a053909289
fuy7ye-svg <fuy7ye@gmail.com>
2026-02-26 01:37:31 +0300

    Add files via upload

---
+++ مجلد جديد/main.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as: {bot.user.name}')
+
+@bot.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.channels, name='✈️welcome')
+    if channel:
+        embed = discord.Embed(
+            
+            description=f"**حيّاك الله** {member.mention}",
+            color=0x2f3136
+        )
+        embed.set_image(url=member.display_avatar.url)
+        await channel.send(embed=embed)
+
+# تأكد من وضع التوكن الجديد هنا
+bot.run('MTQ3NjI2ODY3NDc4Nzg0MDIwMg.GKtcA-.PAcZdR_J1AEwJu8HCUspB0ECShqLDsJBsA7DY4')
